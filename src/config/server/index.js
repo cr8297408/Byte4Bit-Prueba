82f941404a2a947b438a68bd3e85f1c2aa2835ca
Cesar Ruiz <96269153+cr8297408@users.noreply.github.com>
2022-12-01 20:21:37 -0500

    feat: :art: init project
    init project configuration

---
+++ src/config/server/index.js
@@ -0,0 +1,24 @@
+const http = require("http");
+const app = require("./server");
+
+const server = http.createServer(app);
+const db = require("../connection/connectBd");
+
+const port = app.get("port");
+
+async function dbConnection() {
+  try {
+    await db.sequelize.sync();
+    console.log("Database connect");
+    require("../socket.io");
+    server.listen(port, () => {
+      console.log("APP LISTENING IN PORT: ", port);
+    });
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+dbConnection();
+
+module.exports = server;
