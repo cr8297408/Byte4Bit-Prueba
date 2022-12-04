6bfc9346f5abedeb7e13cf62cb6fe5acfc4463de
cr8297408 <cr8297408@gmail.com>
2022-12-04 18:12:32 -0500

    perf(routes): :truck: redirect user to documentation
    if user enter in url, redirect to url/api-docs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const config = require('../config/env');
 
 const CategorieRouter = require('./categories.route');
 const CommentRouter = require('./comments.route');
@@ -8,11 +9,16 @@ const AuthRouter = require('./auth.route');
 
 const router = Router();
 
+const path = `${config.URL_SWAGGER}/api-docs`;
+
 router.use('/v1/auth', AuthRouter);
 router.use(expressJWT);
 router.use('/v1/films', FilmRouter);
 router.use('/v1/categories', CategorieRouter);
 router.use('/v1/comments', CommentRouter);
+router.use('/', (req, res) => {
+  res.redirect(path)
+})
 
 module.exports = {
   router,
